11499449fa2d49ab521f2aa395c0bc1f5cdd375b
Mahema Singh <mahemafire99@gmail.com>
2019-03-22 09:21:37 -0400

    Data muse might be better lol

--- Main.py
+++ Main.py
@@ -1,16 +1,30 @@
 import requests
 import json
 
+
+
 app_id = "6ce9309d"
 app_key = "b815139ed48d09f44ffb8f7c7a66a0bf"
 
+
+
 language = 'en'
-word_id = 'selfie'
+target_language = "es"
+word_id = 'orange'
 
-url = "https://od-api.oxforddictionaries.com/api/v1" + language + "/" + word_id.lower()
 
-r = requests.get(url)
+#url Normalized frequency
+definition_url = 'https://od-api.oxforddictionaries.com:443/api/v1/wordlist/' + language + '/' +word_id
+r = requests.get(definition_url, headers = {'app_id' : app_id, 'app_key' : app_key})
+status_code = ("code {}\n".format(r.status_code))
+textfile = "text \n" + r.text
+json_string=json.dumps(r.json(), indent =4, separators=(",", ":"))
 
-print("code {}\n".format(r.status_code))
-print("text \n" + r.text)
-print("json \n"+ json.dumps(r.json()))
+
+data = json.loads(json_string)
+print(data , '\n')
+
+
+word = data['results'][0]["id"]
+print ('word:', word)
+
